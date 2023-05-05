c0be0e715d8f3f100cc5271f26e82259e3eff515
SHANE RILEY <shaneriley0013@gmail.com>
2023-05-05 14:38:02 -0500

    add crud, validation, images

--- app/controllers/demons_controller.rb
+++ app/controllers/demons_controller.rb
@@ -1,5 +1,55 @@
 class DemonsController < ApplicationController
+  
   def index
     @demons = Demon.all
   end
+
+  def show
+    @demon = Demon.find(params[:id])
+  end
+
+  def new
+    @demon = Demon.new
+  end
+
+  def create
+    @demon = Demon.new(demon_params)
+
+    if @demon.save
+      redirect_to @demon
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @demon = Demon.find(params[:id])
+  end
+
+  def update
+    @demon = Demon.find(params[:id])
+
+    if @demon.update(demon_params)
+      redirect_to @demon
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @demon = Demon.find(params[:id])
+    @demon.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def demon_params
+      params.require(:demon).permit(:name, :title, :description, :image_url)
+    end
+
 end
+
+
+
+
--- app/models/demon.rb
+++ app/models/demon.rb
@@ -1,2 +1,10 @@
 class Demon < ApplicationRecord
+
+  validates :name, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+
+
+
 end
